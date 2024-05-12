8b37ee893e79315741964e3fa4d9c41810cdd498
dombean <46692370+dombean@users.noreply.github.com>
2024-05-12 12:31:30 +0100

    fix github repo link in setup.sh

--- setup.sh
+++ setup.sh
@@ -13,9 +13,9 @@ sudo apt-get install --yes git
 
 # clone repository if not already cloned
 if [ -d ~/ansible_ubuntu_install ]; then
-  echo "$HOME/ansible_ubuntu_install already cloned! Make sure it is a clone of: http://github.com/dombean/ansible_ubuntu_install.git"
+  echo "$HOME/ansible_ubuntu_install already cloned! Make sure it is a clone of: https://github.com/dombean/ansible"
 else
-  git clone http://github.com/dombean/ansible_ubuntu_install.git ~/ansible_ubuntu_install
+  git clone https://github.com/dombean/ansible ~/ansible_ubuntu_install
 fi
 
 cd ~/ansible_ubuntu_install
