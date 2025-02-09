a5e10ead557821297a805d7e193ea8aad763a718
dombean <46692370+dombean@users.noreply.github.com>
2025-02-09 00:24:36 +0000

    update open_yazi.sh

--- mac_scripts/open_yazi.app/Contents/MacOS/open_yazi.sh
+++ mac_scripts/open_yazi.app/Contents/MacOS/open_yazi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Open Ghostty and ensure it runs with your full zsh configuration
+# Open Ghostty and ensure it runs with full zsh configuration
 open -na "Ghostty" --args -e "zsh -c 'source ~/.zshrc; y; exec zsh'"
